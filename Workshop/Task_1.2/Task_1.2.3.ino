0ae5b5e07b9c3cf90c98557cfa9e23e0628e61e7
Bijja Tanmay Karthik <ed25b015@smail.iitm.ac.in>
2026-02-10 23:23:37 +0530

    add

---
+++ Workshop/Task_1.2/Task_1.2.3.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>    
+#include <Adafruit_SSD1306.h> 
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.println("Hello World");
+  
+  display.drawRect(0, 30, 50, 20, WHITE);
+  display.display();
+}
+
+void loop() {}
